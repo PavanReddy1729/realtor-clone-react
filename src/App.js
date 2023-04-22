260e0e8bc24771b52b997c1ca4543d1811bccd60
Your Name <you@example.com>
2023-04-22 12:26:09 -0700

    create pages ad routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
     <div>
-      <h1>hello world</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
